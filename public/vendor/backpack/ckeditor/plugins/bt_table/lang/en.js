8aa705351b1bdceffb67cbb663b98af1ddacf99e
Kirill Kovalenko <kyryll.kovalenko@gmail.com>
2017-08-22 11:32:43 +0300

    mini fix

--- public/vendor/backpack/ckeditor/plugins/bt_table/lang/en.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-CKEDITOR.plugins.setLang( 'bt_table', 'en', {
-    "compactStyle": "Compact style",
-    "addBorders": "Add borders",
-    "addStripes": "Add stripes",
-    "addHover": "Add hover effect",
-});
